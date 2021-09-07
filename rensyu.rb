f09577427c43ef7e6c4ca34e9e1d033c9a6ea755
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2021-09-08 08:46:15 +0900

    電車乗り換え

---
+++ rensyu.rb
@@ -0,0 +1,46 @@
+puts ['Fizz'][3 % 3]
+
+puts ['Fizz'][2]
+
+puts '数字くれ'
+
+number = gets.to_i
+
+number.times do
+  a = 'hello world'
+  a.freeze
+
+  puts a
+end
+
+number1 = gets.to_i
+
+if (number1 % 3).zero? && number1 % 5 != 0
+  print('Fizz')
+elsif (number1 % 5).zero? && number1 % 3 != 0
+  print('Buzz!')
+else
+  (number1 % 3).zero? && (number1 % 5).zero?
+  print('FizzBuzz')
+end
+start_time = Time.now
+
+def fizz_buzz(num)
+  ret = "#{['Fizz'][num % 3]}#{['Buzz'][num % 5]}"
+  ret.empty? ? num.to_s : ret
+end
+
+puts((1..100).collect { |num| fizz_buzz num })
+puts [1, 2, 3].class
+puts(1..3)
+class BookApp
+  WRITE_REVIEW = 0
+  DISPLAY_REVIEW = 1
+  END_PROGRAM = 2
+end
+
+p "処理概要 #{Time.now - start_time}s"
+
+puts '素数の入力'
+
+print('hello ,Ruby.')
